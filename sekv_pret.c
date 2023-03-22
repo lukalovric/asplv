10c8e2ac1186f581944cc888c1821da21e5dd88d
lukalovric <105853522+lukalovric@users.noreply.github.com>
2023-03-22 03:13:18 +0100

    Update sekv_pret.c

--- sekv_pret.c
+++ sekv_pret.c
@@ -15,7 +15,7 @@ int sekv_pret(float V[], int n, float x) {
 int main() {
     time_t  t1, t2;
     srand((unsigned)time(NULL));
-    int n = rand() % 1000000;  // size of array
+    int n = rand() % 1000000;
     float* V = (int*)malloc(n * sizeof(int));
     if (V == NULL) {
         return -1;
@@ -32,10 +32,10 @@ int main() {
     t2 = clock();
 
     if (result == -1) {
-        printf("Element not found.\n");
+        printf("Element nije pronaden.\n");
     }
     else {
-        printf("Element found at index %d.\n", result);
+        printf("Element pronaden na %d.\n", result);
     }
     printf("Vrijeme trajanja je % dms\n", t2 - t1);
     printf("broj elemenata %d", n);
